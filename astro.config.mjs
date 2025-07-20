eab9d54d7f533dbf6ffc921adea041229fc8bc56
romaingbearhoneyco <roamingbearhoneyco@gmail.com>
2025-07-20 15:29:43 -0400

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://roamingbearhoneyco.githug.io',
   vite: {
     plugins: [tailwindcss()]
   }
