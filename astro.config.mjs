2f8c99774935329b2dd3ac71f7fbeedc44b2b8b2
romaingbearhoneyco <roamingbearhoneyco@gmail.com>
2025-07-20 16:10:53 -0400

    modified astro.config.mjs to have subdomain as well

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://testsite.roamingbearhoneyco.com',
+  site: 'https://roamingbearhoneyco.com',
   vite: {
     plugins: [tailwindcss()]
   }
